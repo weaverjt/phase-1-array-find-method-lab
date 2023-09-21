cfbea7a4cd0c6a82ec9191315010e775a6a5f34b
Jacob <weaver.jacob.t@gmail.com>
2023-09-20 19:50:20 -0500

    func

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+let record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+   
+
+//need a function called superBowl
+
+function superbowlWin(record) {
+ let fuu = record.find(item => item.result === "W")
+ if (fuu === undefined)
+ {
+  return undefined;
+ }
+ return fuu.year;
+    }
+
